4ff56e9e1097924cfbfcca8e6cc792cb1c07d038
Gus <garciagus1992@gmail.com>
2025-08-29 15:55:42 +0200

    Add modelos ensamblados
    Voting classifier hard and soft

---
+++ modelos_ensamblados.py
@@ -0,0 +1,52 @@
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_moons
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# creamos un dataset con make_moons
+X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+
+
+# entrenamos 3 modelos distintos de regresión logística, random forest classifier y support vector classifier
+log_clf = LogisticRegression(solver="lbfgs", random_state=42)
+rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+svm_clf = SVC(gamma="scale", random_state=42)
+
+# hacemos la clasificación por voto y entrenamos los modelos por separado
+voting_clf = VotingClassifier(
+    estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)],
+    voting='hard')
+
+voting_clf.fit(X_train, y_train)
+
+
+# hacemos un for para los 3 modelos y printamos la predicción de los 3
+print("Hard voting classifier: \n")
+for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
+
+
+# hacemos lo mismo que antes pero cambiamos el parámetro del clasificador de voto de hard a soft
+log_clf = LogisticRegression(solver="lbfgs", random_state=42)
+rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+svm_clf = SVC(gamma="scale", probability=True, random_state=42)
+
+voting_clf = VotingClassifier(
+    estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)],
+    voting='soft')
+voting_clf.fit(X_train, y_train)
+
+# y mostramos el resultado
+print("\n")
+print("Soft voting classifier: \n")
+for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
