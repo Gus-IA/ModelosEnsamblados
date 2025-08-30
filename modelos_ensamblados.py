4d66be8a3b50c1fe86faf3faeaae3d89c384908e
Gus <garciagus1992@gmail.com>
2025-08-30 12:21:40 +0200

    Add bagging y pasting  y árbol de decisión

--- modelos_ensamblados.py
+++ modelos_ensamblados.py
@@ -5,6 +5,17 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.datasets import fetch_openml
+import matplotlib as mpl
+
 
 # creamos un dataset con make_moons
 X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
@@ -50,3 +61,98 @@ for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
+
+
+
+# ---- Bagging y Pasting ----
+
+# instanciamos el BagginsClassifier pasándole como parámetro el árbol de decisión para ensamblar
+bag_clf = BaggingClassifier(
+    DecisionTreeClassifier(random_state=42), n_estimators=500,
+    max_samples=100, bootstrap=True, random_state=42, oob_score=True)
+bag_clf.fit(X_train, y_train)
+y_pred = bag_clf.predict(X_test)
+
+
+# mostramos las prediciones y la precisión
+print(accuracy_score(y_test, y_pred))
+print(bag_clf.oob_score_)
+
+# entrenamos y mostramos el resultado de un modelo con solo árbol de decisión
+tree_clf = DecisionTreeClassifier(random_state=42)
+tree_clf.fit(X_train, y_train)
+y_pred_tree = tree_clf.predict(X_test)
+print(accuracy_score(y_test, y_pred_tree))
+
+
+# mostramos el gráfico del árbol de decisión y el mismo pero con bagging
+def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.45, -1, 1.5], alpha=0.5, contour=True):
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+    if contour:
+        custom_cmap2 = ListedColormap(['#7d7d58','#4c4c7f','#507d50'])
+        plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", alpha=alpha)
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs", alpha=alpha)
+    plt.axis(axes)
+    plt.xlabel(r"$x_1$", fontsize=18)
+    plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
+
+fix, axes = plt.subplots(ncols=2, figsize=(10,4), sharey=True)
+plt.sca(axes[0])
+plot_decision_boundary(tree_clf, X, y)
+plt.title("Decision Tree", fontsize=14)
+plt.sca(axes[1])
+plot_decision_boundary(bag_clf, X, y)
+plt.title("Decision Trees with Bagging", fontsize=14)
+plt.ylabel("")
+plt.show()
+
+
+# --- Árbol de decisión ---- 
+
+# instanciamos un modelo de árbol de decisión y lo entrenamos
+rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, random_state=42)
+rnd_clf.fit(X_train, y_train)
+
+y_pred_rf = rnd_clf.predict(X_test)
+
+# cargamos el dataset iris y entrenamos el radom forest
+# y usamos el feature importances para ver la importancia relativa de cada una de las características
+iris = load_iris()
+rnd_clf = RandomForestClassifier(n_estimators=500, random_state=42)
+rnd_clf.fit(iris["data"], iris["target"])
+for name, score in zip(iris["feature_names"], rnd_clf.feature_importances_):
+    print(name, score)
+
+
+
+
+# cargamos el dataset mnist y entrenamos un random forest
+mnist = fetch_openml('mnist_784', version=1)
+mnist.target = mnist.target.astype(np.uint8)
+
+rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+rnd_clf.fit(mnist["data"], mnist["target"])
+
+
+
+# mostramos el gráfico
+def plot_digit(data):
+    image = data.reshape(28, 28)
+    plt.imshow(image, cmap = mpl.cm.hot,
+               interpolation="nearest")
+    plt.axis("off")
+
+
+plot_digit(rnd_clf.feature_importances_)
+
+cbar = plt.colorbar(ticks=[rnd_clf.feature_importances_.min(), rnd_clf.feature_importances_.max()])
+cbar.ax.set_yticklabels(['Not important', 'Very important'])
+plt.show()
+
