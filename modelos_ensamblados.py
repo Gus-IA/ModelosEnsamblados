a9056fe0191326b562b807a4bfbb21a369d2a69d
Gus <garciagus1992@gmail.com>
2025-08-31 12:30:21 +0200

    add boosting

--- modelos_ensamblados.py
+++ modelos_ensamblados.py
@@ -15,6 +15,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 from sklearn.datasets import fetch_openml
 import matplotlib as mpl
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost
 
 
 # creamos un dataset con make_moons
@@ -155,4 +162,177 @@ plot_digit(rnd_clf.feature_importances_)
 cbar = plt.colorbar(ticks=[rnd_clf.feature_importances_.min(), rnd_clf.feature_importances_.max()])
 cbar.ax.set_yticklabels(['Not important', 'Very important'])
 plt.show()
+
+
+
+# ---- Boosting ----
+
+# ---- Ada Boost ----
+
+# instanciamos el clasificador AdaBoost
+# usando el árbol de decisión como modelo base
+ada_clf = AdaBoostClassifier(
+    DecisionTreeClassifier(max_depth=1), n_estimators=200,
+    algorithm="SAMME", learning_rate=0.5, random_state=42)
+ada_clf.fit(X_train, y_train)
+
+
+# mostramos el gráfico
+plot_decision_boundary(ada_clf, X, y)
+plt.show()
+
+
+# ---- Gradient Boosting ----
+
+# creamos unos datos aleatorios y mostramos el gráfico
+X = np.random.rand(100, 1) - 0.5
+y = 3*X[:, 0]**2 + 0.05 * np.random.randn(100)
+plt.plot(X,y,'.g')
+plt.show()
+
+# instanciamos un árbol de decisión y entrenamos los datos creados
+tree_reg1 = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg1.fit(X, y)
+
+
+# podemos calcular errores de las prediciones 
+# y mostramos el gráfico
+y2 = y - tree_reg1.predict(X)
+plt.plot(X,y2,'.g')
+
+# entrenamos de nuevo pero con los errores de la predición anterior
+tree_reg2 = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg2.fit(X, y2)
+
+
+# y podemos obtener el error del primer modelo sobre el segundo modelo
+# y mostramos el gráfico
+y3 = y2 - tree_reg2.predict(X)
+plt.plot(X,y3,'.g')
+
+# entrenamos un tercer modelo
+tree_reg3 = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg3.fit(X, y3)
+
+# mostramos el gráfico de todos los modelos
+def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style="b.", data_label=None):
+    x1 = np.linspace(axes[0], axes[1], 500)
+    y_pred = sum(regressor.predict(x1.reshape(-1, 1)) for regressor in regressors)
+    plt.plot(X[:, 0], y, data_style, label=data_label)
+    plt.plot(x1, y_pred, style, linewidth=2, label=label)
+    if label or data_label:
+        plt.legend(loc="upper center", fontsize=16)
+    plt.axis(axes)
+
+plt.figure(figsize=(11,11))
+
+plt.subplot(321)
+plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h_1(x_1)$", style="g-", data_label="Training set")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.title("Residuals and tree predictions", fontsize=16)
+
+plt.subplot(322)
+plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1)$", data_label="Training set")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.title("Ensemble predictions", fontsize=16)
+
+plt.subplot(323)
+plot_predictions([tree_reg2], X, y2, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_2(x_1)$", style="g-", data_style="k+", data_label="Residuals")
+plt.ylabel("$y - h_1(x_1)$", fontsize=16)
+
+plt.subplot(324)
+plot_predictions([tree_reg1, tree_reg2], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1)$")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+plt.subplot(325)
+plot_predictions([tree_reg3], X, y3, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_3(x_1)$", style="g-", data_style="k+")
+plt.ylabel("$y - h_1(x_1) - h_2(x_1)$", fontsize=16)
+plt.xlabel("$x_1$", fontsize=16)
+
+plt.subplot(326)
+plot_predictions([tree_reg1, tree_reg2, tree_reg3], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1) + h_3(x_1)$")
+plt.xlabel("$x_1$", fontsize=16)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+plt.show()
+
+
+
+# podemos usar gradientBoostingRegresor para hacer lo mismo sin ir uno a uno
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=3, learning_rate=1.0, random_state=42)
+gbrt.fit(X, y)
+
+gbrt_slow = GradientBoostingRegressor(max_depth=2, n_estimators=200, learning_rate=0.1, random_state=42)
+gbrt_slow.fit(X, y)
+
+fix, axes = plt.subplots(ncols=2, figsize=(10,4), sharey=True)
+
+# mostramos el gráfico
+plt.sca(axes[0])
+plot_predictions([gbrt], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="Ensemble predictions")
+plt.title("learning_rate={}, n_estimators={}".format(gbrt.learning_rate, gbrt.n_estimators), fontsize=14)
+plt.xlabel("$x_1$", fontsize=16)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+plt.sca(axes[1])
+plot_predictions([gbrt_slow], X, y, axes=[-0.5, 0.5, -0.1, 0.8])
+plt.title("learning_rate={}, n_estimators={}".format(gbrt_slow.learning_rate, gbrt_slow.n_estimators), fontsize=14)
+plt.xlabel("$x_1$", fontsize=16)
+
+plt.show()
+
+
+
+# podemos encontrar el número óptimo de árboles usando early stopping
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
+
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=200, random_state=42)
+gbrt.fit(X_train, y_train)
+
+errors = [mean_squared_error(y_val, y_pred)
+          for y_pred in gbrt.staged_predict(X_val)]
+bst_n_estimators = np.argmin(errors) + 1
+
+gbrt_best = GradientBoostingRegressor(max_depth=2, n_estimators=bst_n_estimators, random_state=42)
+gbrt_best.fit(X_train, y_train)
+
+
+# mostramos el gráfico
+min_error = np.min(errors)
+
+fix, axes = plt.subplots(ncols=2, figsize=(10,4), sharey=True)
+
+plt.subplot(121)
+plt.plot(errors, "b.-")
+plt.plot([bst_n_estimators, bst_n_estimators], [0, min_error], "k--")
+plt.plot([0, 200], [min_error, min_error], "k--")
+plt.plot(bst_n_estimators, min_error, "ko")
+plt.text(bst_n_estimators, min_error*1.2, "Minimum", ha="center", fontsize=14)
+plt.axis([0, 200, 0, 0.01])
+plt.xlabel("Number of trees")
+plt.ylabel("Error", fontsize=16)
+plt.title("Validation error", fontsize=14)
+
+plt.subplot(122)
+plot_predictions([gbrt_best], X, y, axes=[-0.5, 0.5, -0.1, 0.8])
+plt.title("Best model (%d trees)" % bst_n_estimators, fontsize=14)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.xlabel("$x_1$", fontsize=16)
+
+plt.show()
+
+
+# --- XGBOOST ----
+
+
+# instanciamos el xgboost
+# lo entrenamos y hacemos prediciones
+# y printamos los valores de mean squared error
+xgb_reg = xgboost.XGBRegressor(random_state=42)
+xgb_reg.fit(X_train, y_train)
+y_pred = xgb_reg.predict(X_val)
+val_error = mean_squared_error(y_val, y_pred)
+print("Validation MSE:", val_error)          
+
 
